048ae4e38d99b3307b2242d48980a84ec35e2387
Ondrej Sika <ondrej@ondrejsika.com>
2026-01-22 12:42:39 +0000

    feat(azure_with_modules): Create TF workspace for example with modules

---
+++ azure_with_modules/_terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.57.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "200acaec-2d60-43ad-915a-e8f5352a4ba7"
+}
