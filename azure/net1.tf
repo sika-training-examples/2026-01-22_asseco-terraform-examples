a7bc18cba7dc225a15df1d775c1d701244eb11eb
Ondrej Sika <ondrej@ondrejsika.com>
2026-01-22 12:15:50 +0000

    feat(azure): Peer net1 and net2

--- azure/net1.tf
+++ azure/net1.tf
@@ -14,6 +14,14 @@ resource "azurerm_virtual_network" "net1" {
   address_space = ["10.10.0.0/16"]
 }
 
+resource "azurerm_virtual_network_peering" "net1-to-net2" {
+  resource_group_name = azurerm_resource_group.training.name
+  name                = "${azurerm_resource_group.training.name}-peer-net1-to-net2"
+
+  virtual_network_name      = azurerm_virtual_network.net1.name
+  remote_virtual_network_id = azurerm_virtual_network.net2.id
+}
+
 resource "azurerm_subnet" "net1-default" {
   resource_group_name  = azurerm_resource_group.training.name
   virtual_network_name = azurerm_virtual_network.net1.name
--- azure/net2.tf
+++ azure/net2.tf
@@ -14,6 +14,14 @@ resource "azurerm_virtual_network" "net2" {
   address_space = ["10.20.0.0/16"]
 }
 
+resource "azurerm_virtual_network_peering" "net2-to-net1" {
+  resource_group_name = azurerm_resource_group.training.name
+  name                = "${azurerm_resource_group.training.name}-peer-net2-to-net1"
+
+  virtual_network_name      = azurerm_virtual_network.net2.name
+  remote_virtual_network_id = azurerm_virtual_network.net1.id
+}
+
 resource "azurerm_subnet" "net2-default" {
   resource_group_name  = azurerm_resource_group.training.name
   virtual_network_name = azurerm_virtual_network.net2.name
